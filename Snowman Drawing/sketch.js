99c5ee396c7dd035fac6b3d9b24b07d68f3ecb70
Brady Gould <48325596+MhsBGould@users.noreply.github.com>
2019-03-21 10:00:58 -0400

    Add files via upload

---
+++ Snowman Drawing/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+createCanvas(400,400)
+}
+
+function draw() {
+  background(0, 255, 255);
+  fill(73, 199, 48);
+  // The ground
+  rect(0, 300, 400, 100);
+
+
+  // The sun
+  fill(183, 194, 31);
+  ellipse(80, 64, 100, 100);
+
+  // The snowman
+  fill(255, 255, 255);
+  ellipse(200, 300, 150, 150);//bottom
+  ellipse(200, 200, 100, 100);//middle
+  ellipse(200, 120, 75, 75);//top
+
+  //The Arms
+  line(160, 200, 25, 199);//left arm
+line(240, 200, 75, 100);//right arm
+
+}
