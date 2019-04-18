a8c4c776c261622f26b008753fa212f6ede482eb
Brady Gould <48325596+MhsBGould@users.noreply.github.com>
2019-04-17 22:57:58 -0400

    Update sketch.js

--- My First Project/sketch.js
+++ My First Project/sketch.js
@@ -2,7 +2,7 @@ var mySound; // Creates a variable to hold the sound
 //var myImage:
 
  function preload() {
-   mySound = loadSound('dancin.mp3');  // Loads the sound file into the variable
+   mySound = loadSound('dancin.mp3.mp3');  // Loads the sound file into the variable
    //my Image = loadImage('dance.jpg'); //Loads the image
  }
 
