957fa2320c94be4381b6626b4a82cecd35ca107b
Brady Gould <48325596+MhsBGould@users.noreply.github.com>
2019-04-17 23:17:04 -0400

    Add files via upload

--- Using The Mouse/sketch.js
+++ Using The Mouse/sketch.js
@@ -1,29 +1,23 @@
-/* Mouse following using automatic function calls.
-Brady Gould*/
+/* Mouse following using built-in variables
+Brady*/
 let size = 50;
 
 function setup() {
-  var firstCanvas = createCanvas(600, 600);  // create a square window for drawing
+  var secondCanvas = createCanvas(600, 600);  // create a square window for drawing
 }
 
 function draw() {
-  // Resets the size after it reaches 600
-  if (size > 600) {
+  line(mouseX, mouseY, 20, 20); // Draw a line
+  rectMode(CENTER)
+  rect(mouseX, mouseY,size,size);
+  if (mouseIsPressed) {
+    
+    // When the mouse button is pressed
+    // change the colour randomly
+    fill(random(255), random(255), random(255));
+    size = size+2;
+  }
+  if (size > 100){//reset after the size reaches 100
     size = 10;
   }
-}
-
-function mouseMoved() {
-  // This function is called whenever the mouse is moved
-    background(127);
-  rect(mouseX, mouseY, size, size); // Draw a rectangle
-
-}
-
-function mouseClicked() { //click mouse to make the rectangle bigger
-  size = size+10
-}
-
-function mouseWheel() { //spin wheel while mouse is moving to change color
-    fill(random(255), random(255), random(255));
 }
