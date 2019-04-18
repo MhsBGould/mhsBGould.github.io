a5dbc7ae24f41f80305fd65de89dd0be8b50f0db
Brady Gould <48325596+MhsBGould@users.noreply.github.com>
2019-04-17 22:20:32 -0400

    Add files via upload

---
+++ My First Project/sketch.js
@@ -0,0 +1,12 @@
+var mySound; // Creates a variable to hold the sound
+
+ function preload() {
+   mySound = loadSound('dancin.mp3');  // Loads the sound file into the variable
+ }
+
+ function setup() {
+   createCanvas(640, 480);
+   // Notice that you have to put the variable name in front of the functions
+   mySound.setVolume(0.1);
+   mySound.play();
+ }
