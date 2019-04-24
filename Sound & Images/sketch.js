dac5463452c0e2c3b9bcb9729a88b30797a42a2f
Brady Gould <48325596+MhsBGould@users.noreply.github.com>
2019-04-24 08:28:08 -0400

    Update sketch.js

--- Sound & Images/sketch.js
+++ Sound & Images/sketch.js
@@ -8,7 +8,7 @@ var img; //Creates a variable to hold the image
 
   function setup() {
     createCanvas(1280,800);
-      background(random(0,255),random(0,255),random(0,255));
+     background(random(0,255),random(0,255),random(0,255));
 
     // Notice that you have to put the variable name in front of the functions
     mySong.setVolume(0.1);
