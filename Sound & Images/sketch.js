382e1f4941d1501d9a2942d1195105c573e4df89
Brady Gould <48325596+MhsBGould@users.noreply.github.com>
2019-04-25 10:12:41 -0400

    Update sketch.js

--- Sound & Images/sketch.js
+++ Sound & Images/sketch.js
@@ -1,10 +1,12 @@
 var mySong; // Creates a variable to hold the sound
 var img, loadimg; //Creates a variable to hold the image
+var bg;
 
   function preload() {
     mySong = loadSound('song.mp3');  // Loads the sound file into the variable
     loadimg = loadImage('tenor.gif');
     img = createImg('tenor.gif');
+    bg = loadImage('wall.png');
   }
 
   function setup() {
@@ -17,5 +19,6 @@ var img, loadimg; //Creates a variable to hold the image
   }
 
   function draw() {
+     background(bg);
      img.position(0,0);
   }
