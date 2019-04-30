0e35ffd21db9b233667a6c11e02d2f7bea1e78fd
Brady Gould <48325596+MhsBGould@users.noreply.github.com>
2019-04-30 09:37:28 -0400

    Add files via upload

--- Desicion Structure/sketch.js
+++ Desicion Structure/sketch.js
@@ -2,30 +2,60 @@
 var x=1280;
 var y=700;
 var bg;
-var yoshi;
-var textSize;
+var hero;
+var bubble;
+var myFont;
+var level = 0;
 
 function preload(){
    bg = loadImage('8bit.png');
-   yoshi = loadImage('yoshi.png')
+   hero = loadImage('yoshi.png')
+   bubble = loadImage('bubble.png')
+   myFont = loadFont('eight-bit-dragon.otf')
 }
 
 function setup() {
   // put setup code here
   createCanvas(x,y);//canvas
+  background(bg);//background image
 }
 
 function draw() {
   // put drawing code here
-  background(bg);//background image
-  textSize(50);//text size
-  textFont('Georgia')//text font
-  text('PRESS W TO START',x/3.4,y/2)  //Makes text appear
-}
-
-function keyTyped() {
-  if (key === 'w') {
-    textSize = 0;
-    image(yoshi, x/4, y/1.2);
+  /*------------------START------------------------*/
+  if (level===0) {
+    textSize(50);
+    textFont(myFont)//text font
+    text('PRESS ENTER TO START',x/3.4,y/2)  //Makes text appear
+    if (keyIsDown(ENTER)) {
+      level+=1;
+    }
   }
+
+  /*------------------INSTRUCTIONS------------------------*/
+  else if (level===1) {
+    background(bg);
+    image(hero,x/10,y/2);
+    image(bubble,x/-30,y/10);
+    textSize(25);
+    textFont(myFont)//text font
+    text('Welcome to the game',x/3.7,y/2.7)//Make text appear
+    text('press ENTER to start your own adventure',x/7,y/2.4)   //Makes text appear
+    if (keyIsDown(ENTER)) {
+      level+=1;
+    }
+  }
+
+  /*------------------GAME------------------------*/
+  else if (level===2) {
+    background(bg);
+    image(hero,x/10,y/2);
+
+    if (keyIsDown(RIGHT_ARROW)) {
+      x+=5
+    }
+
+
+  }
+  print(level)
 }
