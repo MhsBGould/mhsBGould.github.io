877bba2f863e9c963a18fe168a118031ab3c1af1
Brady Gould <48325596+MhsBGould@users.noreply.github.com>
2019-05-08 09:05:37 -0400

    Update sketch.js

--- Sound & Images/sketch.js
+++ Sound & Images/sketch.js
@@ -1,9 +1,11 @@
+//Global Variables
 var mySong; // Creates a variable to hold the sound
 var img, loadimg; //Creates a variable to hold the image
 var bg;
 
   function preload() {
-    mySong = loadSound('song.mp3');  // Loads the sound file into the variable
+    //loads the sounds and images
+    mySong = loadSound('song.mp3');
     loadimg = loadImage('tenor.gif');
     img = createImg('tenor.gif');
     bg = loadImage('wall.png');
@@ -13,12 +15,13 @@ var bg;
     createCanvas(windowWidth,windowHeight);
      background(random(0,255),random(0,255),random(0,255));
 
-    // Notice that you have to put the variable name in front of the functions
+    //My Song is commanded to play
     mySong.setVolume(0.1);
     mySong.play();
   }
 
   function draw() {
-     background(bg);
-     img.position(windowWidth/2,windowHeight/4);
+    //Gif is displayed
+    background(bg);
+    img.position(windowWidth/2,windowHeight/4);
   }
